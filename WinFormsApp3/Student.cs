30c79df8b640da3610100e4a825bae1bf7dcb384
t.novruzov <t.novruzov@STHQ0124-01>
2022-09-30 09:15:15 +0400

    Add project files.

---
+++ WinFormsApp3/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp3
+{
+    class Student
+    {
+        public static int StaticId { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public Student(string? name)
+        {
+            Id = ++StaticId;
+            Name = name;
+        }
+
+
+        public override string ToString()
+         => $"{Id} - {Name}";
+    }
+}
